fa812a78372dca469549770b7629a2fe6c7a9a42
AnyaLV <134580003+AnyaLV@users.noreply.github.com>
2023-07-16 22:46:20 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,4 +1,9 @@
---Задача 1
+-- Задача customers_count
+-- Данный запрос считает количество уникальных покупателей из таблицы customers
+SELECT count(DISTINCT customer_id) as customers_count
+FROM customers
+
+--Задача top_10_total_income
 -- В данном запросе произведено объединение трех таблиц с целью определить количество сделок их сумму по каждому продавцу.
 -- Далее получившиеся результаты были отсортированы в порядке убывания и выбраны первые 10 продавцов. 
 select e.first_name||' '||e.last_name as name,
@@ -10,7 +15,7 @@ group by 1
 order by income desc
 limit 10
 
--- Второй вариант решения задачи 1
+-- Второй вариант решения задачи top_10_total_income
 with table1 as (
 				select e.first_name||' '||e.last_name as name,
 				count (distinct sales_id) as operations,
@@ -24,8 +29,7 @@ select name,operations,income
 from table1
 where rn <=10
 
-
---Задача 2
+--Задача lowest_average_income
 -- В данном запросе произведено объединение трех таблиц для расчета выручки по каждому продавцу. 
 -- Далее получившиеся результаты сравнили со средним значением выручки по всем продавцам и оставили только те, которые ниже среднего.
 with table1 as (
@@ -40,10 +44,9 @@ from table1
 where income < (select avg(income)from table1)
 order by income asc
 
---Задача 3
+--Задача day_of_the_week_income
 -- В данном запросе произведено объединение трех таблиц для расчета выручки по каждому продавцу в разрезе дня недели.
--- Чтобы корректно отсортировать результат дополнительно добавлена колонка с номером недели.
- 
+-- Чтобы корректно отсортировать результат дополнительно добавлена колонка с номером недели. 
 select name,week_day,income
 from (
 		select e.first_name||' '||e.last_name as name,
